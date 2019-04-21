bd9d54352ae5f8dcced37b5215976209258f46d5
iulliauta <44622321+iulliauta@users.noreply.github.com>
2019-04-21 08:08:37 -0400

    Add files via upload

--- views/app.js
+++ views/app.js
@@ -1,29 +1,24 @@
 var app = angular.module('catsvsdogs', []);
 var socket = io.connect({transports:['polling']});
 
-var bg1 = document.getElementById('background-stats-1');
-var bg2 = document.getElementById('background-stats-2');
-
 app.controller('statsCtrl', function($scope){
-  $scope.aPercent = 50;
-  $scope.bPercent = 50;
 
   var updateScores = function(){
-    socket.on('scores', function (json) {
+    socket.on('zbor', function (json) {
        data = JSON.parse(json);
-       var a = parseInt(data.a || 0);
-       var b = parseInt(data.b || 0);
+       tableB = document.getElementById('tab-zboruri');
+       tableH = document.getElementById("tab-head");
+       tableB.innerHTML = '';
+       tableB.appendChild(tableH);
 
-       var percentages = getPercentages(a, b);
+       //Data = array de zboruri
+       for(let i = 0; i < data.lenght; i++) {
+         let plecare = data[i].plecare,
+             intoarcere = data[i].intoarcere,
+             zbor = data[i].zbor;
 
-       bg1.style.width = percentages.a + "%";
-       bg2.style.width = percentages.b + "%";
-
-       $scope.$apply(function () {
-         $scope.aPercent = percentages.a;
-         $scope.bPercent = percentages.b;
-         $scope.total = a + b;
-       });
+         addRowToTable(plecare, intoarcere, zbor);
+       }
     });
   };
 
@@ -35,6 +30,29 @@ app.controller('statsCtrl', function($scope){
     init();
   });
 });
+
+function addRowToTable(plecare, intoarcere, zbor) {
+    tableB = document.getElementById("tab-zboruri");
+    row = document.createElement("tr");
+    // create 3 td elements for row cells 
+    cellPlecare = document.createElement("td");
+    cellIntoarcere = document.createElement("td");
+    cellZbor = document.createElement("td");
+
+    // create info for cells
+    infoPlecare = document.createTextNode(plecare);
+    infoIntoarcere = document.createTextNode(intoarcere);
+    infoZbor = document.createTextNode(zbor);
+
+    //add info to cells
+    cellPlecare.appendChild(infoPlecare);
+    cellIntoarcere.appendChild(infoIntoarcere);
+    cellZbor.appendChild(infoZbor);
+
+    //add row to table
+    tableB.appendChild(row);
+     
+}
 
 function getPercentages(a, b) {
   var result = {};
